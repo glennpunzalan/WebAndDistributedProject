d9830c7ce0a2d2500e23f7a816832ba6fc5358b1
glennpunzalan <glennpunzalan@lewisu.edu>
2024-12-09 19:27:11 -0600

    commit

--- pokemon.js
+++ pokemon.js
@@ -14,6 +14,40 @@ fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
   })
   .catch((error) => console.error('Error fetching Pokémon:', error));
 
-function fetchPokemonDataBeforeRedirect(id) {
+async function fetchPokemonDataBeforeRedirect(id) {
+  try {
+    const [pokemon, pokemonSpecies] = await Promise.all([
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => res.json()),
+      fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) => res.json())
+    ]);
+
+    console.log(pokemon, pokemonSpecies);
+
+  return true
+
+  } catch (error) {
+    console.error('Error fetching Pokémon data before redirect:', error);
+  }
+}
+
+function displayPokemons(pokemon) {
+    listWrapper.innerHTML = "";
+
+    pokemon.forEach((pokemon) => {
+        const pokemonID = pokemon.url.split("/")[6];
+        const listItem = document.createElement("div");
+        listItem.className = "list-item";
+        listItem.innerHTML = `
+            <div class="number-wrap">
+                <p class="caption-fonts">#${pokemonID}</p>
+            </div>
+            <div class="img-wrap">
+                <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg" alt="${pokemon.name}" />
+            </div>
+            <div class="name-wrap">
+                <p class="body3-fonts">#${pokemon.name}</p>
+            </div>
+        `;
+    })
 
 }
